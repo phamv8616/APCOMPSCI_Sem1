b56cf58306a9a00e33b16d8d026aeec97c23d3fb
Vyvian Pham <phamvyvian@gmail.com>
2016-09-07 13:51:34 -0700

    Lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,28 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 3;
+		int num2 = 4;
+		int num3 = num1 * num2;
+		
+		System.out.println(+num1 +" multiplied by " +num2 +" equals " +num3);
+		
+		String first = "Vyvian";
+		String last = "Pham";
+		String address = "2468 Torrey Pines Rd";
+		String city = "San Diego";
+		String zip = "92130";
+		
+		System.out.println(first +" " + last +"," +" " +address +"," +" " +city +"," +" "+zip);
+		
+		int l = 3;
+		int w = 4;
+		int h = 5;
+		
+		int surfacea = 2 * ( l * w) + 2 * ( h * w ) + 2 * (l * h);
+		
+		System.out.println("The surface area of your rectangle is " +surfacea);
+	}
+}	
+	
