fa470127d66963c7ed0856635ceae37fefac5d53
phamv8616 <phamvyvian@gmail.com>
2016-10-17 14:33:14 -0700

    In-Class 10/17/16

--- APLesson_05/APLab_05.2/adventure.java
+++ APLesson_05/APLab_05.2/adventure.java
@@ -4,6 +4,12 @@ public class adventure
 	public static void main(String[]args)
 	{
 		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Once upon a time there was a young boy that went to McDonalds." +
+							"\nHe had trouble figuiring out what he wanted to order.");
+
+		String choice = keyboard.nextLine();
 		
 	}
+	
+	
 }
