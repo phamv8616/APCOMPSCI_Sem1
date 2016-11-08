be6b3ac107429d9fc85d5553e4be64e68e85b849
phamv8616 <phamvyvian@gmail.com>
2016-11-08 14:32:46 -0800

    In-Class 11/8/16

--- APLesson_08/APLab_08/Center.java
+++ APLesson_08/APLab_08/Center.java
@@ -3,17 +3,16 @@ public class Center
 {
 	public static void main(String[]args)
 	{
-		// Scanner keyboard = new Scanner(System.in);
-		// System.out.println("Please enter word#1");
-		// String word1 = keyboard.nextLine();
-		// System.out.println("Please enter word#2");
-		// String word2 = keyboard.nextLine();	
-		// System.out.println("Please enter word#3");
-		// String word3 = keyboard.nextLine();
-		// makeCenter(word1);
-		// makeCenter(word2);
-		// makeCenter(word3);
-		String word = "POTATO";
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter word#1");
+		String word1 = keyboard.nextLine();
+		System.out.println("Please enter word#2");
+		String word2 = keyboard.nextLine();	
+		System.out.println("Please enter word#3");
+		String word3 = keyboard.nextLine();
+		makeCenter(word1);
+		makeCenter(word2);
+		makeCenter(word3);
 	}
 	public static String makeCenter(String word)
 	{
