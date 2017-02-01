2a36937af8cf8ecfcadbb1274cce2b47cc7a2955
phamv8616 <phamvyvian@gmail.com>
2017-02-01 14:35:08 -0800

    In Class 2/1/17

---
+++ AP Lesson_13/CountingMachine.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CountingMachine
+{
+	public static void main(String[]args)
+	{
+		ArrayList<Money> inventory = new ArrayList<~>();
+		
+		inventory.add(new Coin("Penny", "2.5g", 0.01));
+		inventory.add(new Coin("Nickel", "5.0g", 0.05));
+		inventory.add(new Coin("Dime", "2.268g", 0.10));
+		inventory.add(new Coin("Quarter", "5.678g", 0.25));
+		inventory.add(new Bill("One", "Washington", 01.00));
+		inventory.add(new Bill("Five", "Lincoln", 5.00));
+		inventory.add(new Bill("Ten", "Hamilton", 10.00));
+		inventory.add(new Bill("Twenty", "Jackson", 20.00));
+		inventory.add(new Bill("Hundred", "Franklin", 100.00));
+		
+		// for(Money x : inventory)
+		// {
+			// System.out.println(x);
+		// }
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter your bills and coins...");
+		String cash = keyboard.nextLine();
+		
+		Scanner scan = new Scanner(cash);
+		
+		while(scan.hasNext())
+		{
+			String x = scan.next();
+			
+			for(Money b : inventory)
+			{
+				if(b.scan().equals(x) || b.getName().equals(x))
+				{
+					b.setCount(b.getCount() + 1);
+				}
+			}
+		}
+		
+		for(Money x : inventory)
+		{
+			total += x.getValue() * x.getCount();
+		}
+		
+		System.out.println(total);
+	}
+	
+}
---
+++ AP Lesson_13/Money.java
@@ -0,0 +1,24 @@
+public class Money
+{
+	private String name;
+	private double value;
+	
+	public Money()
+	{
+		this.name = "";
+		this.value = 0;
+	}
+	
+	public Money(String n, double v)
+	{
+		this.name = n;
+		this.value = v;
+	}
+	
+	public abstract String scan();
+	
+	public String toString()
+	{
+		return "Your " + name + " is worth $" + value + "\n";		
+	}
+}
