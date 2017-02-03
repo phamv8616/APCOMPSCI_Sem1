6b0180870a6fa66c40e704532a0aa15e0302aee7
phamv8616 <phamvyvian@gmail.com>
2017-02-03 14:32:22 -0800

    In Class 2/3/17

--- AP Lesson_13/Money.java
+++ AP Lesson_13/Money.java
@@ -1,20 +1,42 @@
-public class Money
+public abstract class Money
 {
 	private String name;
 	private double value;
+	private int count;
 	
 	public Money()
 	{
 		this.name = "";
 		this.value = 0;
+		this.count = 0;
 	}
 	
 	public Money(String n, double v)
 	{
 		this.name = n;
 		this.value = v;
+		this.count = 0;
 	}
 	
+	public void setCount(int c)
+	{
+		this.count = c;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getValue()
+	{
+		return value;
+	}
+	
+	public int getcount()
+	{
+		return count;
+	}
 	public abstract String scan();
 	
 	public String toString()
